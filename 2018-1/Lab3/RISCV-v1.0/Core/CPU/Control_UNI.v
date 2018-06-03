83345c2fc3f50a02b0c47ca40e8199b9a75d532e
verasthiago <thiago.veras.machado@gmail.com>
2018-06-03 23:50:51 +0000

    90% Uni control done

--- 2018-1/Lab3/RISCV-v1.0/Core/CPU/Control_UNI.v
+++ 2018-1/Lab3/RISCV-v1.0/Core/CPU/Control_UNI.v
@@ -4,24 +4,22 @@
  */
 
  module Control_UNI(
-    input  wire [6:0]  iOp, 
-//	 input wire [6:0]  iFunct7,
-//    input  wire [2:0]  iFunct3,
+    input  wire [6:0]  iOp, iFunct7,
+    input  wire [2:0]  iFunct3,
     output wire [1:0]  oALUop,
-    output wire oBranch, oMemRead, oMemtoReg, oMemWrite, oALUsrc, oRegWrite, oOrigWrite
+    output wire oBranch, oMemRead, oMemtoReg, oMemWrite, oALUsrc, oRegWrite
 );
 
 
 initial
 begin
-	oALUsrc = 1'b0;
+	oALUsrc   = 1'b0;
 	oMemtoReg = 1'b0;
 	oRegWrite = 1'b0;
-	oMemRead = 1'b0;
+	oMemRead  = 1'b0;
 	oMemWrite = 1'b0;
-	oBranch = 1'b0;
-	oOrigWrite = 1'b0;
-	oALUop = 2'b0;
+	oBranch   = 1'b0;
+	oALUop    = 2'b0;
 end
 
 
@@ -30,15 +28,66 @@ begin
 	case(iOp)
         OPCRTYPE:
 	        begin
-		    	oALUsrc = 1'b0;
+		    	oALUsrc   = 1'b0;
 				oMemtoReg = 1'b0;
 				oRegWrite = 1'b1;
-				oMemRead = 1'b0;
+				oMemRead  = 1'b0;
 				oMemWrite = 1'b0;
-				oBranch = 1'b0;
-				oOrigWrite = 1'b0;	// 0 o sinal vem da ula/memoria. 1 o sinal vem do pc+imm
-				oALUop = 2'b10;
+				oBranch   = 1'b0;
+				oALUop    = 2'b10;
 	        end
+	    OPCIMM,
+	    OPCLOGICIMM:
+	    	begin
+	    		oALUsrc   = 1'b1;
+				oMemtoReg = 1'b0;
+				oRegWrite = 1'b1;
+				oMemRead  = 1'b0;
+				oMemWrite = 1'b0;
+				oBranch   = 1'b0;
+				oALUop    = 2'b11;
+	    	end
+	    OPCLOAD:
+	    	begin
+	    		oALUsrc   = 1'b1;
+				oMemtoReg = 1'b1;
+				oRegWrite = 1'b1;
+				oMemRead  = 1'b1;
+				oMemWrite = 1'b0;
+				oBranch   = 1'b0;
+				oALUop    = 2'b00;
+	    	end
+	    OPCBRANCH:
+	    	begin
+	    		oALUsrc   = 1'b0;
+				oMemtoReg = 1'b0;
+				oRegWrite = 1'b0;
+				oMemRead  = 1'b0;
+				oMemWrite = 1'b0;
+				oBranch   = 1'b1;
+				oALUop    = 2'b11;	    		
+	    	end
+	    OPCSTORE:
+	    	begin
+	    		oALUsrc   = 1'b0;
+				oMemtoReg = 1'b0;
+				oRegWrite = 1'b0;
+				oMemRead  = 1'b0;
+				oMemWrite = 1'b0;
+				oBranch   = 1'b1;
+				oALUop    = 2'b11;	    	
+	    	end
+	    OPCLUI,
+	    OPCAUIPC:
+	    	begin
+	    		oALUsrc   = 1'b1;
+				oMemtoReg = 1'b0;
+				oRegWrite = 1'b1;
+				oMemRead  = 1'b0;
+				oMemWrite = 1'b0;
+				oBranch   = 1'b0;
+				oALUop    = 2'b00;	    	
+			end 
     endcase
 end
 
