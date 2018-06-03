1b555b15ba7caeb52118354585081199d49c2b84
GabrielBessa <gabrielbessarofl@gmail.com>
2018-06-03 13:45:01 +0000

    Datapath fully compiling, merged with the other group

--- 2018-1/Lab3/RISCV-v1.0/Core/CPU/Datapath_UNI.v
+++ 2018-1/Lab3/RISCV-v1.0/Core/CPU/Datapath_UNI.v
@@ -1,177 +1,251 @@
-/*
- * Caminho de dados processador Uniciclo
- *
- */
+/* Top Level Entity - RISCV Custom*/ 
+
+`ifndef PARAM
+	`include "Parametros.v"
+`endif
 
 module Datapath_UNI (
-    // Inputs e clocks
-    input  wire        iCLK, iCLK50, iRST,
-    input  wire [31:0] iInitialPC,
-
-    // Para monitoramento
-    output wire [31:0] wPC, woInstr,
-    input  wire [4:0]  wRegDispSelect,
-    output wire [31:0] wDebug,
-
-    output wire        wCRegWrite,
-    output wire [1:0]  wCALUOp,
-    output wire [2:0]  wCMem2Reg,
-	 
-	 output wire [31:0] wBRReadA,
-	 output wire [31:0] wBRReadB,
-	 output wire [31:0] wBRWrite,
-	 output wire [31:0] wULA,	 
-
-
-    //  Barramento de Dados
-    output             DwReadEnable, DwWriteEnable,
-    output      [3:0]  DwByteEnable,
-    output      [31:0] DwAddress, DwWriteData,
-    input       [31:0] DwReadData,
-
-    // Barramento de Instrucoes
-    output             IwReadEnable, IwWriteEnable,
-    output      [3:0]  IwByteEnable,
-    output      [31:0] IwAddress, IwWriteData,
-    input       [31:0] IwReadData,
-
-    input [7:0] iPendingInterrupt                           // feito no semestre 2013/1 para implementar a deteccao de excecoes (COP0)
-    );
+	input iClk,
+	input iClr,
+	input [31:0] iInst,
+	output wire [31:0] wA, wB,
+	output reg oZero,
+	output reg [31:0] oResult,
+	output wire [31:0] wPC,
+	output wire [31:0] wReg,
+	output wire [31:0] wUla
+	
+/*	
+//	output reg [6:0] iOpcode,  
+//	output reg [9:0] Funct10,
+//	output reg [11:0] iImmTipoI,
+//	output reg [11:0] iImmTipoS,
+//	output reg [11:0] iImmTipoSB,
+//	output reg [19:0] iImmTipoU,
+*Ja foram declarados nas linhas seguintes como wire, pelo menos foi o que vi.
+*/
+);
 
 
+/* =============== esquema instrucoes =========*/
+/*
+TIPO R
+wInst[6:0] - opcode
+wInst[11:7] - rd
+wInst[14:12] funct3
+wInst[19:15] - rs1
+wInst[24:20] - rs2
+wInst[31:25] funct7
 
-/* Padrao de nomeclatura
- *
- * XXXXX - registrador XXXX
- * wXXXX - wire XXXX
- * wCXXX - wire do sinal de controle XXX
- * memXX - memoria XXXX
- * Xunit - unidade funcional X
- * iXXXX - sinal de entrada/input
- * oXXXX - sinal de saida/output
- */
+TIPO I
+wInst[6:0] - opcode
+wInst[11:7] - rd
+wInst[14:12] funct3
+wInst[19:15] - rs1
+wInst[31:20] - imm[11:0]
 
-reg  [31:0] PC;                                 // registrador do PC
+TIPO S
+wInst[6:0] - opcode
+wInst[11:7] - imm[4:0]
+wInst[14:12] funct3
+wInst[19:15] - rs1
+wInst[24:20] - rs2
+wInst[31:25] - imm[11:5]
+
+TIPO SB
+wInst[6:0] - opcode
+wInst[11:7] - imm[4:1|11]
+wInst[14:12] funct3
+wInst[19:15] - rs1
+wInst[24:20] - rs2
+wInst[31:25] - imm[12|10:5]
+
+TIPO U
+wInst[6:0] - opcode
+wInst[11:7] - rd
+wInst[31:12] imm[31:12]
+
+TIPO UJ
+wInst[6:0] - opcode
+wInst[11:7] - rd
+wInst[31:12] - imm[12|10:1|11|19:12]
+*/
+
+
+/*========== assignments da intrução para os fios ==========*/
+
+wire [6:0] iOpcode;
+wire [9:0] Funct10;
+wire [11:0] iImmTipoI;
+wire [11:0] iImmTipoS;
+wire [11:0] iImmTipoSB;
+wire [19:0] iImmTipoU;
+wire [31:0] oUla;
+
+
+/* =========== fios dos modulos aritmeticos  ================ */
+
+wire [4:0] ctrl_to_ula;		// saída do ALUControl / entrada iControl da ULA
+wire [31:0] ula_to_mux;		// saída da ULA / entrada do multiplexador de saída
+wire [31:0] iB_to_mux;		// saída do registrador iB/ entrada dado0 do multiplexador da ULA
+wire [31:0] mux_to_ula;		// saida do multiplexador da ULA/ entrada do iB da ULA
+wire [31:0] imm_gen;			// saída do gerador de imediato/ entrada dado1 do multiplexador da ULA
+//wire [31:0] pc;				// program counter
+reg [31:0] mem_out = 32'd0;
+
+
+
+/*================== fios do controle ================= */
+
+
+wire oALUSrc;
+wire oMemToReg;
+wire oRegWrite;
+wire oMemRead;
+wire oMemWrite;
+wire oBranch;
+wire [1:0] oALUOp;
+
+/* ===== fios da ula/pc/muxes ====== */
+
+
+
+
+
+/* ======== entradas do banco de registradores ======= */
+
+wire [31:0] iA,iB;
+wire [31:0] mux_to_reg;			// saida do multiplexador da ula para o breg
+wire [4:0] iReadRegister1;
+wire [4:0] iReadRegister2;
+wire [4:0] iWriteRegister;
+wire [4:0] iRegDispSelect, iVGASelect;
+wire [31:0] oRegDisp,oVGARead;
+
+
+wire [31:0] adder_to_pc;		// saída do adder para o pc (pois o pc só é escrito na borda de subida)
 wire [31:0] wPC4;
 wire [31:0] wiPC;
-wire [31:0] wInstr;
-wire [31:0] wMemDataWrite;
-wire [6:0]  wOpcode, wFunct7;
-wire [4:0]  wAddrRs1, wAddrRs2, wAddrRd, wRegDst;     // enderecos dos reg rs,rt ,rd e saida do Mux regDst
-wire [31:0] wOrigALU;
-wire        wZero;
-wire [4:0]  wALUControl;
-wire [31:0] wALUresult, wRead1, wRead2, wMemAccess;
-wire [31:0] wReadData;
-wire [31:0] wDataReg;
-wire [15:0] wImm;
-wire [31:0] wExtImm;
-wire [31:0] wBranchPC;
-wire [31:0] wExtZeroImm;
-wire        wCMemRead, wCMemWrite, wCALUSrc;
-wire [2:0]  wFunct3;
+wire [31:0] wBranch;
+reg [31:0] PC;
+wire [31:0] wInst;
+
+assign wPC4 = PC + 32'h4;
+assign wPC = PC;
+assign wBranch = PC + imm_gen;		// endereco de branch
 
 
 
+assign iOpcode = iInst[6:0];
+assign Funct10[9:3] = iInst[31:25];
+assign Funct10[2:0] = iInst[14:12];
+assign iImmTipoI = iInst[31:20];
+assign iImmTipoS[11:5] = iInst[31:25];
+assign iImmTipoS[4:0] = iInst[11:7];
+//assign iImmTipoSB[12] = iInst[31];
+//assign iImmTipoSB[10:5] = iInst[30:25];
+//assign iImmTipoSB[4:1] = iInst[11:8];
+//assign iImmTipoSB[11] = iInst[7];
+assign iImmTipoU = iInst[31:12];
 
-/* Inicializacao */
-initial
-begin
-    PC         <= BEGINNING_TEXT;
-end
-
-assign wPC4         = wPC + 32'h4;                          /* Calculo PC+4 */
-//assign wBranchPC    = wPC4 + {wExtImm[29:0],{2'b00}};       /* Endereco do Branch */ //A verificar
-assign wPC          = PC;
-assign wOpcode      = wInstr[6:0]; 
-assign wAddrRs1     = wInstr[19:15];
-assign wAddrRs2     = wInstr[24:20];
-assign wAddrRd      = wInstr[11:7];
-assign woInstr      = wInstr;
-assign wFunct3 	  = wInstr[14:12];
-assign wFunct7 	  = wInstr[31:25];
-//assign wImmGen 	  = wInstr[31:0];
-
-/* Assigns para debug */
-assign wDebug   = 32'h00BEBAD0;//005AD1C0//00F1A5C0//0ACEF0DA;
+//assign iA = wA;
+//assign iB = wB;
+assign iReadRegister1 = iInst[19:15];
+assign iReadRegister2 = iInst[24:20];
+assign iWriteRegister = iInst[11:7];
 
 
-/* Barramento da Memoria de Instrucoes */
-assign    IwReadEnable      = ON;
-assign    IwAddress         = wPC;
-assign    IwWriteData       = ZERO;
-assign    wInstr            = IwReadData;
+// Controle Uniciclo
 
-/* Banco de Registradores */
-Registers RegsUNI ( //a modificar
-    .iCLK(iCLK),
-    .iCLR(iRST),
-    .iReadRegister1(wAddrRs1),
-    .iReadRegister2(wAddrRs2),
-    .iWriteRegister(wRegDst),
-    .iWriteData(wDataReg),
-    .iRegWrite(wCRegWrite),
-    .oReadData1(wRead1),
-    .oReadData2(wRead2),
-	);
+Control_UNI iControl (
+		.iOp(iOpcode),
+		.oALUsrc(oALUSrc),
+		.oMemtoReg(oMemToReg),
+		.oRegWrite(oRegWrite),
+		.oMemRead(oMemRead),
+		.oMemWrite(oMemWrite),
+		.oBranch(oBranch),
+		.oALUop(oALUOp)
+);
 
-/* ALU CTRL */
-ALUControl ALUControlunit (
-    .iFunct3(wFunct3),
-	 .iFunct7(wFunct7),
-    .iOpcode(wOpcode),
-    .iALUOp(wCALUOp),
-    .oControlSignal(wALUControl)
-	);
 
-/* ALU */
-ALU ALUunit(
-    .iCLK(iCLK),
-    .iRST(iRST),
-    .iControlSignal(wALUControl),
-    .iA(wRead1),
-    .iB(wOrigALU),
-    .oALUresult(wALUresult),
-    .oZero(wZero),
-	);
+/* ================ Banco de registradores ================= */
 
-MemStore MemStore0 (
-    .iAlignment(wALUresult[1:0]),
-    .iWriteTypeF(STORE_TYPE_DUMMY),
-//    .iOpcode(wOpcode), //Comentado pois n tem essa funcao no arq memStore.v, mas pode ser util dps
-    .iData(wRead2),
-    .oData(wMemStore),
-    .oException()
-	);
+Registers regs (
+	.iCLK(iClk),
+	.iCLR(iClr),
+	.iRegWrite(oRegWrite),
+	.iReadRegister1(iReadRegister1),
+	.iReadRegister2(iReadRegister2),
+	.iWriteRegister(iWriteRegister),
+	.iRegDispSelect(iRegDispSelect),
+	.iWriteData(mux_to_reg),
+	.oReadData1(iA),
+	.oReadData2(iB),
+	.oRegDisp(oRegDisp),
+	.iVGASelect(iVGASelect),
+	.oVGARead(oVGARead)
+);
 
-/* Barramento da memoria de dados */
-assign DwReadEnable     = wCMemRead;
-assign DwWriteEnable    = wCMemWrite;
-assign DwWriteData      = wMemDataWrite;
-assign wReadData        = DwReadData;
-assign DwAddress        = wALUresult;
 
-MemLoad MemLoad0 (
-    .iAlignment(wALUresult[1:0]),
-    .iLoadTypeF(LOAD_TYPE_DUMMY),
-//    .iOpcode(wOpcode), //Comentado pois n tem essa funcao no arq memLoad.v, mas pode ser util dps
-    .iData(wReadData),
-    .oData(wMemAccess),
-    .oException()
-	);
+/*  =============== modulos aritmeticos ======================*/
 
-/* Unidade de Controle */
-Control_UNI CtrUNI (
-    .iFunct3(wFunct3),
-	 .iFunct7(wFunct7),
-	 .iOp(wOpcode),
-    .oBranch(wBranchPC), //Nao sei se isso ta certo ou se eh pra ser wCBranch apesar de nao estar declarado.
-    .oMemRead(wCMemRead),	 
-    .oMemtoReg(wCMem2Reg),
-    .oALUop(wCALUOp),
-    .oMemWrite(wCMemWrite) ,
-	 .oALUsrc(wCALUSrc),
-    .oRegWrite(wCRegWrite),	 
-	);
 
+// ALU Control
+
+ALUControl aluControlUnit (
+		.iFunct3(iFunct3),
+		.iFunct7(iFunct7),
+		.iALUOp(iALUOp),
+		.ALUCtrl(ctrl_to_ula)
+);
+
+
+// Mux2x1(32 bits) - saída do iB BREG e entrada na iB da ULA (0-Read Data 2/1-ImmGen)
+// TODO: Adicionar o gerador de imediato no dado1
+mux32_2 muxReg(	
+	.sel(oALUSrc),
+	.dado0(iB),
+	.dado1(iImmTipoI),
+	.saida(mux_to_ula)
+);
+
+
+// ALU
+
+ALU alu0 (
+	.iControlSignal(ctrl_to_ula),
+	.iA(iA),
+	.iB(mux_to_ula),
+	.oZero(oZero),
+	.oALUresult(ula_to_mux)
+);
+
+// Mux2x1(32 bits) - saída da ULA e entrada do BREG (1-saída da ULA/ 2-saída da memória de dados)
+mux32_2 muxUla (
+	.sel(oMemToReg),
+	.dado0(mem_out),
+	.dado1(ula_to_mux),
+	.saida(mux_to_reg)
+);
+
+/* somador do endereco de branch*/
+
+
+//mux32_2 muxPc (
+//	.sel(oBranch),
+//	.dado0(wPC4),
+//	.dado1(wBranch),
+//	.saida(wPC)
+//);
+always @(posedge iClk)
+	begin
+		PC <= wPC4;
+	end
+	
+//always @(*)
+//	begin
+//		wUla <= mux_to_out;
+//	end
+		
 endmodule
