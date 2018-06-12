8bf17e0c6fe4445adcc45623c0ba3e84331f876b
VerasThiago <thiago.wortnnes@gmail.com>
2018-06-12 14:55:39 -0300

    Ordem dos assing erradas [FIXED]

--- 2018-1/Lab4/RISCV-v1.0/Core/CPU/Control_MULTI.v
+++ 2018-1/Lab4/RISCV-v1.0/Core/CPU/Control_MULTI.v
@@ -5,32 +5,10 @@
 module Control_MULTI (
 	/* I/O type definition */
 	input wire iCLK, iRST,
-	input wire [5:0] iOp, iFunct,
-	input wire [4:0] iFmt, iRt,		// 1/2016. Adicionado iRt.
-	input wire iFt,
-	output wire oIRWrite, oMemtoReg, oMemWrite, oMemRead, oIorD, 
-					oPCWrite, oPCWriteBEQ, oPCWriteBNE,oRegWrite, oRegDst,
-					oFPPCWriteBc1t, oFPPCWriteBc1f, oFPRegWrite, oFPFlagWrite, 
-					oFPU2Mem, 
-	output wire [1:0] oALUOp, oALUSrcA, oFPDataReg, oFPRegDst,
-	output wire [2:0] oALUSrcB, oPCSource, oStore,
-	output wire [5:0] oState,
-	//Adicionado em 1/2014
-	output wire [2:0] oLoadCase,
-	output wire [1:0] oWriteCase,
-	// feito no semestre 2013/1 para implementar a deteccao de excecoes (COP0)
-	input iCOP0ALUoverflow, iCOP0FPALUoverflow, iCOP0FPALUunderflow, 
-			iCOP0FPALUnan, iCOP0UserMode, iCOP0ExcLevel,
-	input [7:0] iCOP0PendingInterrupt,
-	output oCOP0PCOriginalWrite,
-	output reg oCOP0RegWrite, oCOP0Eret, oCOP0ExcOccurred,
-	output oCOP0BranchDelay,
-	output [4:0] oCOP0ExcCode,
-	output oCOP0Interrupted,
-	// 2017/1
-	input iFPBusy,
-	output oFPStart
-	);
+	input wire [5:0] iOp,
+	output wire  
+	output wire [1:0] oALUOp, oOriBALU, oMem2Reg,
+);
 
 
 wire [14:0] word;			// sinais de controle do caminho de dados
@@ -39,20 +17,19 @@ wire [5:0] nx_state;		// next estate
 
 
 
-assign oPCcondWrite  = word[0];
-assign oPCwrite      = word[1];
-assign oIorD         = word[2];
+assign oMem2Reg      = word[1:0];
+assign oRegWrite     = word[2];
 assign oMemWrite     = word[3];
-assign oMemRead      = word[4];
-assign oIRWrite      = word[5];
-assign oOriPC        = word[6];
-assign oALUop        = word[8:7];
-assign oOriAALU      = word[9];
-assign oOriBALU      = word[11:10];
-assign oRegWrite     = word[12];
-assign oMem2Reg      = word[14:13];
+assign oPCcondWrite  = word[4];
+assign oALUop        = word[6:5];
+assign oIorD         = word[7];
+assign oPCwrite      = word[8];
+assign oOriPC        = word[9];
+assign oMemRead      = word[10];
+assign oOriBALU      = word[12:11];
+assign oIRWrite      = word[13];
+assign oOriAALU      = word[14];
 
-assign oState		 = pr_state;
 
 initial
 begin
@@ -134,7 +111,7 @@ begin
 		
 		CONC-LOAD:
 			begin
-				word		<= 15'b10100XXX0X0XX0X;
+				word		<= 15'b101000000000000;
 				nx_state	<= FETCH;
 			end	
 	endcase
